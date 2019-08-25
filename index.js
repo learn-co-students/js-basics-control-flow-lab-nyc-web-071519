a189f3234531ebb1f95c0a4ff83780689a1f78ee
Andrew Santos <asantos@alumni.nd.edu>
2019-08-25 10:39:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+    return 'This one is on me!';
+  }
+  else if (distance > 2500) {
+    return 'No can do.';
+  }
+  else if (distance > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let message;
+  city === "NYC" ? (message = "Ok, sounds good.") : (message = "No go.");
+  return message;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(generosity){
+  switch (generosity) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+
+  }
 }
