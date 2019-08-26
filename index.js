812943dcd1368ff7b43f9b58c0150ecd886d6cb4
github username <github email address>
2019-08-26 17:42:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function scuberGreetingForFeet(ride){
   // Write your code here!
-  if (ride > 2500) {
+  if (ride <= 400) {
+    return "This one is on me!"
+  } else if (ride > 2500) {
     return "No can do."
   } else if (ride > 2000) {
     return "I will gladly take your thirty bucks."
-  } else if (ride <= 400) {
-    return "This one is on me!"
   }
 }
 
